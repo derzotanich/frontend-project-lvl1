62531e7a161fbdf535bf3ceefdc2a8e057cc2669
tanich <derzotanich@gmail.com>
2022-07-18 16:48:21 +0300

    brain-even game created

---
+++ bin/brain-even.js
@@ -0,0 +1,29 @@
+import readlineSync from 'readline-sync';
+import greeting from '../src/cli.js';
+
+const userName = greeting();
+
+console.log('Answer "yes" if the number is even, otherwise answer "no".');
+const arr = [15, 6, 7];
+let lastAnswer = '';
+for (let i = 0; i < arr.length; i += 1) {
+  console.log(`Question: ${arr[i]}`);
+  let rightAnswer = 'no';
+  if (arr[i] % 2 === 0) {
+    rightAnswer = 'yes';
+  }
+  const answer = readlineSync.question('');
+  if (answer === rightAnswer) {
+    console.log('Correct!');
+    lastAnswer = 'correct';
+  } else {
+    lastAnswer = 'incorrect';
+    console.log('"yes" is wrong answer ;(. Correct answer was "no".');
+    console.log(`Let's try again, ${userName}!`);
+    break;
+  }
+}
+
+if (lastAnswer === 'correct') {
+  console.log(`Congratulations, ${userName}!`);
+}
--- src/cli.js
+++ src/cli.js
@@ -6,6 +6,8 @@ const greeting = () => {
   const userName = readlineSync.question('May I have your name? ');
 
   console.log(`Hello, ${userName}!`);
+
+  return userName;
 };
 
 export default greeting;
